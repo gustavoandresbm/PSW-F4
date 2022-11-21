c6a2518ca15506e0e31a7f2024cd2563688832c6
moniktv <54922544+moniktv@users.noreply.github.com>
2022-11-20 23:03:44 -0500

    Update consulta_producto.php

---
+++ php/consulta_producto.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Consultar</title>
+    <title>Consultar</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+</head>
+<body>
+
+ <?php
+
+require('ConexionBD.php');
+
+$codpro = $_POST['codigo'];
+
+
+$sql = "SELECT * FROM personas WHERE id=$codpro";
+$result = mysqli_query($conexion, $basedatos);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+
+
+
+?>
+
+<!-- The Modal -->
+  
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Datos</h4>
+          <button class="close" onclick="location.href='../index.html'">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          
+ 		<?php
+        echo "Codigo: " . $row["codigo"]
+        . "<br> Nombreproducto: " . $row["nombreproducto"]
+        . "<br> Marcaproducto: " . $row["Marcaproducto"]
+        . "<br> Precioproducto: ". $row["precioproducto"]
+        . "<br> Cantidadproducto". $row["cantidadproducto"] 
+        . "". "<br>";
+        
+		?>
+
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button class="btn btn-danger" onclick="location.href='../index.html'">Cerrar</button>
+        </div>
+        
+      </div>
+    </div>
+
+
+
+ <?php
+
+
+        
+//$myJSON = json_encode($row);
+
+//echo $myJSON;
+
+    }
+
+
+} else {
+
+
+
+?>
+
+<!-- The Modal -->
+  
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Error</h4>
+          <button class="close" onclick="location.href='../index.html'">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+          
+ 		<?php
+        echo "Esa producto no existe " . "<br>";
+		?>
+
+
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button class="btn btn-danger" onclick="location.href='../index.html'">Cerrar</button>
+        </div>
+        
+      </div>
+    </div>
+
+
+
+ <?php
+
+
+}
+
+mysqli_close($conexion);
+?> 
